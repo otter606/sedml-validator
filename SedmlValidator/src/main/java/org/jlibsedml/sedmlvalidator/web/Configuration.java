3918cdde28b14f7c9990a06685691a68332ef000
otter606 <ra22597@gmail.com>
2016-01-09 18:29:43 +0000

    initial commit

---
+++ SedmlValidator/src/main/java/org/jlibsedml/sedmlvalidator/web/Configuration.java
@@ -0,0 +1,8 @@
+package org.jlibsedml.sedmlvalidator.web;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class Configuration {
+
+}
---
+++ SedmlValidator/src/main/java/org/jlibsedml/sedmlvalidator/web/SedmlValidatorController.java
@@ -0,0 +1,24 @@
+package org.jlibsedml.sedmlvalidator.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class SedmlValidatorController {
+	
+	   public static void main(String[] args) throws Exception {
+	        SpringApplication.run(SedmlValidatorController.class, args);
+	    }
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+ 
+}
